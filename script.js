44286314a8068530b93b283e7b10022f842dc101
JAOUAD ICHI <71087285+jaouadichi@users.noreply.github.com>
2021-03-02 15:43:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: "2000",
+    reset: "true",
+});
+
+sr.reveal('.header');
+sr.reveal('.hero-box', {delay: 400});
+sr.reveal('.left', {delay: 200});
+sr.reveal('.right', {delay: 200});
+sr.reveal('.logo', {delay: 500});
+
+
+sr.reveal('.featured');
+sr.reveal('.card-1', {delay: 100});
+sr.reveal('.card-2', {delay: 300});
+sr.reveal('.card-3', {delay: 500});
+sr.reveal('.btn-cta', {delay: 200});
+
+
+sr.reveal('.specials');
+sr.reveal('.slid-1', {delay: 300});
+sr.reveal('.slid-2', {delay: 100});
+sr.reveal('.slid-3', {delay: 500});
+
+
+sr.reveal('.for-you');
+sr.reveal('.for-you-text', {delay: 500});
+
+
+sr.reveal('.footer');
+sr.reveal('.useful-links', {delay: 200});
+sr.reveal('.social-links', {interval: 300});
+sr.reveal('.contact', {delay: 400});
+sr.reveal('.about-us', {delay: 600});
+sr.reveal('.terms-links', {delay: 600});
